5f25a8709e53820ba5188e6da8bb6dc3e1dfc86c
Shahzod Rahimov <rakhimovsh3235@gmail.com>
2023-04-04 14:37:26 +0500

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('AkmalExpress CRM')
